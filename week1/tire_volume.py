fed9e000f9d7fe30e0aef2ff7f59ab59aa2a402c
isabelfalin <fal24007@byui.edu>
2026-01-14 14:17:18 -0700

    stretch challenge

--- week1/tire_volume.py
+++ week1/tire_volume.py
@@ -21,8 +21,21 @@ volume = (no_parenthesis * within_parenthesis / 10000000000)
 
 print(f"The approximate volume is {volume:.02f} liters. ")
 
+wants_to_buy = input("Do you want to buy tires with the dimensions you entered? ")
+
+if wants_to_buy.lower() == "yes":
+    phone_number = input("Phone Number: ")
+else:
+    phone_number = ''
+
 current_date_and_time = datetime.now()
 
 with open("volumes.txt", "at") as volumes_file:
-    print(f"{current_date_and_time:%Y-%m-%d}, {width}, {aspect_ratio}, {diameter}, {volume:.02f}", file=volumes_file)
+    print(f"{current_date_and_time:%Y-%m-%d}, {width}, {aspect_ratio}, {diameter}, {volume:.02f}, {phone_number}", file=volumes_file)
 
+
+"""After your program prints the tire volume to the terminal window, your 
+program should ask the user if she wants to buy tires with the dimensions 
+that she entered. If the user answers “yes”, your program should ask for her 
+phone number and store her phone number in the volumes.txt file.
+Ponder"""
