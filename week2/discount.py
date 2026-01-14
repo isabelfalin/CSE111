5ffc82d8fbafb18b60de725e19357792d7358271
isabelfalin <fal24007@byui.edu>
2026-01-13 18:49:29 -0700

    discount assignment

--- week2/discount.py
+++ week2/discount.py
@@ -2,35 +2,32 @@
 your program must subtract 10% from the subtotal. Your program must then 
 compute the total amount due by adding sales tax of 6% to the subtotal."""
 
-# Import the datetime class from the datetime
-# module so that it can be used in this program.
+#importing the function to let me get the date and time from computer
 from datetime import datetime
 
-# Call the now() method to get the current
-# date and time as a datetime object from
-# the computer's operating system.
+#getting the current date and time from the computer
 current_date_and_time = datetime.now()
-
-# Call the weekday() method to get the day of the
-# week from the current_date_and_time object.
 day_of_week = current_date_and_time.weekday()
-
-# Print the day of the week for the user to see.
 print(day_of_week)
 
 sales_tax = None
 
+#create loop that keeps asking for the subtotal
 subtotal = float(input("Please enter the subtotal: "))
 
 if subtotal >= 50 and day_of_week == 1 or 2:
-    subtotal = (subtotal * .10 )
-    total = (.06 * subtotal)
-    sales_tax = (total - subtotal)
-    print(f"Sales tax amount: {sales_tax}")
-    print(f"Total: {total}")
+    discount = (.1 * subtotal)
+    subtotal = (subtotal - discount )
+    sales_tax = (.06 * subtotal)
+    total = (sales_tax + subtotal)
+
+    print(f"Discount amount: {discount:.2f}")
+    print(f"Sales tax amount: {sales_tax:.2f}")
+    print(f"Total: {total:.2f}")
+    
 else:
     total = (sales_tax * subtotal)
-    print(f"Sales tax amount: {sales_tax}")
-    print(f"Total: {total}")
+    print(f"Sales tax amount: {sales_tax:.2f}")
+    print(f"Total: {total:.2f}")
 
 
