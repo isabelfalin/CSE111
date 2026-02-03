bb9102b3f9092ac1c91887b77165555448f811a8
isabelfalin <fal24007@byui.edu>
2026-02-03 14:41:22 -0700

    milestone

--- week5/water_flow.py
+++ week5/water_flow.py
@@ -1,18 +1,21 @@
-def water_column_height():
-    #what do i put here??
-    #do i need to define evry single one? (tower gheith and tank height?)
-    #or are they already defined in the test_water_flower one?
-
-
-
-
-
 def water_column_height(tower_height, tank_height):
-    h = water_column_height
-    t = tower_height
-    w = tank_height
-
-    h = t + 3 * w / 4
+    h = tower_height + 3 * tank_height / 4
+    return h
 
 def pressure_gain_from_water_height(height):
-    p = p * g * h / 1000
+    density = 998.2
+    g = 9.80665
+    pressure = density * g * height / 1000
+    return pressure
+
+#PEMDAS
+
+def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    density = 998.2
+    pressure = -friction_factor * pipe_length * density * fluid_velocity ** 2 / (2000 * pipe_diameter)
+    return pressure
+
+
+
+    
+
