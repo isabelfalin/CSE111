7e1ba3d112f5e7c58427b5dfc6a483a66e7d4c3a
isabelfalin <fal24007@byui.edu>
2026-02-12 12:38:18 -0700

    dumb assignment

--- week6/esteem.py
+++ week6/esteem.py
@@ -39,7 +39,7 @@ def main():
 
     print("1. I feel that I am a person of worth, at least on an equal plane with others.")
     answer = input("Enter D, d, a, or A: ")
-    total_score = total_score + get_score(2, answer)
+    total_score = total_score + get_score(1, answer)
 
     print("2. I feel that I have a number of good qualities")
     answer = input("Enter D, d, a, or A: ")
