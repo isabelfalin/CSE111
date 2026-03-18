4c5d4a44e2c2b66e307281ef779ee45f9caf80db
isabelfalin <fal24007@byui.edu>
2026-03-18 13:09:44 -0600

    peoplesss

---
+++ week11/pupils.py
@@ -0,0 +1,66 @@
+import csv
+
+
+# Each row in the pupils.csv file contains three elements.
+# These are the indexes of the elements in each row.
+GIVEN_NAME_INDEX = 0
+SURNAME_INDEX = 1
+BIRTHDATE_INDEX = 2
+
+get_birthday = lambda student: student[BIRTHDATE_INDEX]
+get_give_name = lambda student: student[GIVEN_NAME_INDEX]
+
+def read_compound_list(filename):
+    """Read the text from a CSV file into a compound list.
+    The compound list will contain small lists. Each small
+    list will contain the data from one row of the CSV file.
+
+    Parameter
+        filename: the name of the CSV file to read.
+    Return: the compound list
+    """
+    # Create an empty list.
+    compound_list = []
+
+    # Open the CSV file for reading.
+    with open(filename, "rt") as csv_file:
+
+        # Use the csv module to create a reader
+        # object that will read from the opened file.
+        reader = csv.reader(csv_file)
+
+        # The first line of the CSV file contains column headings
+        # and not a student's I-Number and name, so this statement
+        # skips the first line of the CSV file.
+        next(reader)
+
+        # Process each row in the CSV file.
+        for row in reader:
+
+            # Append the current row at the end of the compound list.
+            compound_list.append(row)
+
+    return compound_list
+
+
+def print_list(the_list):
+    for line in the_list:
+        print(line)
+
+def main():
+    student_list = read_compound_list('pupils.csv')
+
+    sorted_list = sorted(student_list, key=get_birthday)
+    print()
+    print("Sorted Oldest to Youngest")
+    print_list(sorted_list)
+
+    sorted_list = sorted(student_list, key=get_give_name)
+    print()
+    print("Sorted By Given Name")
+    print_list(sorted_list)
+
+
+if __name__ == "__main__":
+    main()
+    
