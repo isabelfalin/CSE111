1d4f118141f18de63ec42726c9ad10f911ea7f9c
isabelfalin <fal24007@byui.edu>
2026-03-20 14:27:26 -0600

    beginning of tracker

---
+++ final/tracker.py
@@ -0,0 +1,31 @@
+import math, random, pytest
+import csv
+from datetime import datetime, timedelta
+
+def main():
+    pass
+
+def load_players(file_name):
+    pass
+
+def display_menu():
+    pass
+
+def get_menu_choice():
+    pass
+
+def calculate_UTR(players_list):
+    pass
+
+def show_player_stats(players_list):
+    pass
+
+def choose_random_tournament(tournament_list):
+    pass 
+
+def get_qualifying_players(tournament):
+    pass
+
+def display_tournament_info(tournament):
+    pass
+
